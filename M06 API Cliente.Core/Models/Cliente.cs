a4bf74e1d11535eac76082207f4fc3b48ac55a10
Marília Castro <marilialiliu@hotmail.com>
2022-09-09 16:31:29 -0300

    Add project files.

---
+++ M06 API Cliente.Core/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿namespace M06_API_Cliente.Core.Models
+{
+    public class Cliente
+    {
+        public long Id { get; set; }
+
+        public string Cpf { get; set; }
+
+        public string Nome { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public int Idade => DateTime.Now.Year - DataNascimento.Year;
+    }
+}
---
+++ M06 API Cliente/Controllers/ClienteController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace M06_API_Cliente.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClienteController : ControllerBase
+    {
+    }
+}
